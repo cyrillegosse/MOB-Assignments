3cc728e797a9a5a0a4c71504e30acb8a5e955c04
Cyrille GOSSE <cyrillegosse@gmail.com>
2015-10-08 21:56:31 +0700

    TEST

---
+++ MOB-TEST/MOB-TEST/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MOB-TEST
+//
+//  Created by Cyrille GOSSE on 10/8/15.
+//  Copyright © 2015 Cyrille GOSSE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
